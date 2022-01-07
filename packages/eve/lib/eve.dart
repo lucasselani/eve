870c00461b896efd9146dbaf9dc9609f6073e5da
Lucas Selani de Melo <lucas.selani@unico.io>
2022-01-06 22:39:00 -0300

    fix: update modules import in eve package

--- packages/eve/lib/eve.dart
+++ packages/eve/lib/eve.dart
@@ -1,9 +1,11 @@
 library eve;
 
 export 'package:abstractions/abstractions.dart';
+export 'package:dependencies/dependencies.dart';
+export 'package:design_system/design_system.dart';
+export 'package:di/di.dart';
+export 'package:i18n/i18n.dart';
 export 'package:navigator/navigator.dart';
 export 'package:reactive/reactive.dart';
-export 'package:i18n/i18n.dart';
-export 'package:dependencies/dependencies.dart';
-export 'package:di/di.dart';
 export 'src/eve_app.dart';
+export 'src/eve_manager.dart';
--- packages/eve/lib/src/eve_app.dart
+++ packages/eve/lib/src/eve_app.dart
@@ -1,5 +1,4 @@
 import 'package:eve/eve.dart';
-import 'package:eve/src/eve_manager.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
